ddeba81b235546928a30a0b2977b0ee58e0476f5
Sundui-wq <sundui858@gmail.com>
2025-12-09 18:35:38 +0800

    Update Multiplication Javadoc on feature branch

--- src/main/java/lab14/sict/must/edu/mn/Multiplication.java
+++ src/main/java/lab14/sict/must/edu/mn/Multiplication.java
@@ -1,14 +1,14 @@
 package lab14.sict.must.edu.mn;
 
 /**
- * Тооны үржүүлэх үйлдэл хийдэг класс.
+ * Тооны үржүүлэх үйлдэл хийдэг класс (сайжруулсан хувилбар).
  */
-
 public class Multiplication {
     /**
-     * Хоёр тоог үржүүлнэ (хурдан үйлдэл).
+     * Хоёр тоог үржүүлнэ (оновчтой алгоритм).
      */
     public double multiply(double a, double b) {
         return a * b;
     }
 }
+///
