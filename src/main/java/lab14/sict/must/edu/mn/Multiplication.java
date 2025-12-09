2a59ee2b85fd3e6afecbd4ef495880bc6fe79876
Sundui-wq <sundui858@gmail.com>
2025-12-09 18:34:58 +0800

    Update Multiplication Javadoc on develop

--- src/main/java/lab14/sict/must/edu/mn/Multiplication.java
+++ src/main/java/lab14/sict/must/edu/mn/Multiplication.java
@@ -3,14 +3,10 @@ package lab14.sict.must.edu.mn;
 /**
  * Тооны үржүүлэх үйлдэл хийдэг класс.
  */
-public class Multiplication {
 
+public class Multiplication {
     /**
-     * Хоёр тоог үржүүлнэ.
-     *
-     * @param a Эхний тоо
-     * @param b Хоёр дахь тоо
-     * @return Үржвэр
+     * Хоёр тоог үржүүлнэ (хурдан үйлдэл).
      */
     public double multiply(double a, double b) {
         return a * b;
